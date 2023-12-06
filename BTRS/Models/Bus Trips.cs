41d81e4ba3c01e4e430e9c8b3d5702ffa9e5d07d
abdalla wohoush <abdalla.a.w@gmail.com>
2023-12-06 11:15:02 +0300

    added models

---
+++ BTRS/Models/Bus Trips.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTRS.Models
+{
+    public class Bus_Trips
+    {
+
+        [Key]
+        public int TripId { get; set; }
+        public string Destination { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        
+    }
+}
---
+++ BTRS/Models/Bus.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTRS.Models
+{
+    public class Bus
+    {
+        [Key]
+        public int BusId { get; set; }
+        public string CptName { get; set; }
+        public int NOofSeats { get; set;
+    
+    }
+}
